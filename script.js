337e39bc1d97cc0cf3c1a45becc6753012845596
twagar <tanner.agar@gmail.com>
2022-07-13 20:29:45 -0500

    Core functionality completed and working

--- script.js
+++ script.js
@@ -6,37 +6,120 @@ const clearBtn = document.querySelector('[data-all-clear]');
 const prevOpTxt = document.querySelector('[data-previous-op]');
 const currOpTxt = document.querySelector('[data-current-op]');
 
-class calc {
+class Calc {
     constructor(prevOpTxt, currOpTxt) {
         this.prevOpTxt = prevOpTxt;
         this.currOpTxt = currOpTxt;
         this.clear();
     }
-}
 
-function delete() {
 
-}
-
-function clear() {
+clear() {
     this.currentOp = '';
     this.prevOp = '';
     this.operation = undefined;
 }
-const calculator = new calc(prevOpTxt, currOpTxt)
 
-function operate() {
-
+delete() {
+    this.currentOp = this.currentOp.toString().slice(0, -1);
 }
 
-function appendNum(number) {
-
+operate() {
+    let oper;
+    const prev = parseFloat(this.prevOp);
+    const curr = parseFloat(this.currentOp);
+    if (isNaN(prev) || isNaN(curr)) return;
+    switch (this.operation) {
+        case '+':
+            oper = prev + curr;
+            break
+        case '-':
+            oper = prev - curr;
+            break
+        case '÷':
+            oper = prev / curr;
+            break
+        case '*':
+            oper = prev * curr;
+            break
+        default:
+            return
+    }
+    this.currentOp = oper;
+    this.operation = undefined;
+    this.prevOp = '';
 }
 
-function chooseOp(operation) {
-
+appendNum(number) {
+    if (number === '.' && this.currentOp.includes('.')) return;
+    this.currentOp = this.currentOp.toString() + number.toString();
 }
 
-function updateDisp() {
+chooseOp(operation) {
+    if (this.currentOp === '') return
+    if (this.prevOp !== '') {
+        this.operate();
+    }
+    this.operation = operation;
+    this.prevOp = this.currentOp;
+    this.currentOp = '';
+};
 
+updateDisp() {
+    this.currOpTxt.innerText = this.getDispNum(this.currentOp);
+    if (this.operation != null) {
+        this.prevOpTxt.innerText = `${this.getDispNum(this.prevOp)} ${this.operation}`;
+    this.currOpTxt.innerText = this.currentOp;
+} else {
+    this.prevOpTxt.innerText = '';
 }
+}
+
+getDispNum(number) {
+    const stringNum = number.toString();
+    const intDigits = parseFloat(stringNum.split('.')[0]);
+    const deciDigits = stringNum.split('.')[1];
+    let intDisp;
+    if (isNaN(intDigits)) {
+        intDisp = '';
+    } else {
+    intDisp = intDigits.toLocaleString('en', {maximumFractionDigits: 0})
+    }
+    if (deciDigits != null) {
+        return `${intDisp}.${deciDigits}`;
+    } else {
+        return intDisp;
+    }
+}
+}
+clearBtn.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisp();
+});
+
+opBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOp(button.innerText);
+        calculator.updateDisp();
+    })
+});
+
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNum(button.innerText);
+        calculator.updateDisp();
+    });
+});
+
+equalsBtn.addEventListener('click', button => {
+    calculator.operate()
+    calculator.updateDisp();
+});
+
+delBtn.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisp();
+});
+
+
+const calculator = new Calc(prevOpTxt, currOpTxt)
